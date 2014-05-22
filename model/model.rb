ec34e26ec6b7e5e4b0d2a86aa0131c2460112389
Patrick McConlogue <patrick.mcconlogue@gooddata.com>
2014-05-21 17:03:43 -0700

    Second init

--- model/model.rb
+++ model/model.rb
@@ -2,14 +2,14 @@ GoodData::Model::ProjectBuilder.create("Brick Tutorial Project") do |p|
 
     p.add_dataset("employees") do |d|
         d.add_anchor("user_id")
-        d.add_anchor("department")
+        d.add_attribute("department")
         d.add_label("name", :reference => "user_id")
 
     end
 
     p.add_dataset("sales") do |d|
         d.add_fact("amount")
-        d.add_reference("transaction_id", :dataset => 'employees', :reference => 'user_id')
+        d.add_reference("user_id", :dataset => 'employees', :reference => 'user_id')
     end
 
 end
